4cfebffe1113901b3a935ad3febdf475a9ab1cfc
Clovis Santiago <sant_sivolc@hotmail.com>
2022-09-27 14:30:50 -0300

    Primeiro comentario

---
+++ index.js
@@ -0,0 +1,14 @@
+const nomeDoUsuario = prompt("Digite seu nome: ")
+alert ('Seja bem vindo(a), ' + nomeDoUsuario + '!')
+let retornoDoUsuario = confirm ('Deseja continuar, ' + nomeDoUsuario + '?')
+
+
+
+
+
+
+
+
+
+
+
